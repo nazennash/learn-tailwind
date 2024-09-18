b6557b2dd057849dd12ff85937cb1001bc889c2b
Nash Oliver <92800845+nazennash@users.noreply.github.com>
2024-09-18 11:14:59 +0300

    initial-setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./*html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
